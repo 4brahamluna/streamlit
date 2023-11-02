ba72cc292c33d73554cbc4b6999a421d5d032449
4brahamluna <149635644+4brahamluna@users.noreply.github.com>
2023-11-01 21:48:18 -0300

    Update master.py

--- master.py
+++ master.py
@@ -3,23 +3,14 @@ import matplotlib.pyplot as plt
 import folium
 import requests
 
-# Importar os dados do mapa
-dados_mapa = pd.read_csv("mapa_rn.csv")
-
-# Criar um mapa do Rio Grande do Norte
-mapa = folium.Map(location=[-5.18, -36.94], zoom_start=7)
-
-# Adicionar os municípios do Rio Grande do Norte ao mapa
-for municipio in dados_mapa.itertuples():
-
-    # Fazer uma solicitação HTTP ao site do IBGE
-    resposta = requests.get(f"https://geoftp.ibge.gov.br/mapas/bases_cartograficas/malhas_municipais/municipios_2023/png/{municipio.ibge}.png")
-
-    # Armazenar a imagem na memória
-    imagem = resposta.content
-
-    # Adicionar a imagem ao mapa
-    folium.Marker([municipio.latitude, municipio.longitude], popup=municipio.nome, icon=folium.Icon(image=imagem)).add_to(mapa)
-
-# Exibir o mapa
-st.write(mapa)
+@st.cache_data
+def carregar_dados():
+    tabela = pd.read_csv("resultados.csv")
+    return tabela
+with st.container():
+    st.write("---")
+    qtde_dias = st.selectbox("Selecione o período", ["7D", "15D", "21D", "30D"])
+    num_dias = int(qtde_dias.replace("D", ""))
+    dados = carregar_dados()
+    dados = dados[:num_dias]
+    st.area_chart(dados, x="Data", y="Contratos")
